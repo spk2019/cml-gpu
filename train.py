1499679b1018410fcc3b6b115681952aab7dd279
spk2019 <46295017+spk2019@users.noreply.github.com>
2023-04-15 17:44:25 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LogisticRegression
+import torch 
+import torchvision
+
+
+import torch
+import torchvision
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"DEVICE: {torch.cuda.get_device_name()}")
+print(f"GPU AVAILABLE : {torch.cuda.is_available()}")
+print(f"RUNNING ON : {device}")
